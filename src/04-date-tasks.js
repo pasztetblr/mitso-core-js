043550d466944d3dc593b1d76c71e6f83e7799dc
pasztetblr <paxanas18@gmail.com>
2021-12-17 22:01:59 +0300

    completed date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -18,8 +18,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+ function parseDataFromRfc2822( value ) {
+  return Date.parse(value);
 }
 
 /**
@@ -33,8 +33,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601( value ) {
+  return Date.parse(value);
 }
 
 /**
@@ -51,8 +51,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear( date ) {
+  return new Date(date.getFullYear(),1,29).getMonth() == 1;
 }
 
 /**
@@ -70,8 +70,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString( startDate, endDate ) {
+  return new Date(endDate - startDate).toISOString().slice(11, -1);
 }
 
 /**
@@ -90,8 +90,12 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands( date ) {
+  let h = new Date(date).getUTCHours();
+    h = (h < 12) ? h : h - 12;
+    let min = new Date(date).getUTCMinutes();
+    let angle = Math.abs(1/2 * (60 * h - 11 * min));
+    return ((angle > 180) ? 360 - angle : angle) /180 * Math.PI;
 }
 
 /**
@@ -112,8 +116,25 @@ function angleBetweenClockHands(/* date */) {
  *    getDay(365, false) => "December, 31"
  *    getDay(366, true) => "December, 31"
  */
-function getDay(/* day, isLeap */) {
-  throw new Error('Not implemented');
+function getDay( day, isLeap ) {
+  const calendar = {
+    "January": 31,
+    "February": 28+(isLeap?1:0),
+    "March": 31,
+    "April": 30,
+    "May": 31,
+    "June": 30,
+    "July": 31,
+    "August": 31,
+    "September": 30,
+    "October": 31,
+    "November": 30,
+    "December": 31
+  };
+  for (const month of Object.keys(calendar)) {
+    if (day <= calendar[month]) return `${month}, ${day}`;
+    day -= calendar[month];
+  }
 }
 
 module.exports = {
